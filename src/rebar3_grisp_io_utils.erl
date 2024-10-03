067a3c731aab2df8bab65336b6544337b975a8dc
Sebastien Merle <s.merle@gmail.com>
2024-10-03 20:15:51 +0200

    Use rebar3:run/1 to call sub-commands

--- src/rebar3_grisp_io_utils.erl
+++ src/rebar3_grisp_io_utils.erl
@@ -7,17 +7,9 @@
 %--- API -----------------------------------------------------------------------
 
 grisp_pack(RState, Args) ->
-    % TODO remove workaround for profiles
-    OriginalProfile = rebar_state:current_profiles(RState),
-
-    RState1 = rebar_state:current_profiles(RState, [default]),
-
-    case rebar_command(RState1, grisp, pack, Args) of
-        {error, _} = Error ->
-            Error;
-        {ok, RS} ->
-            RS1 = rebar_state:current_profiles(RS, OriginalProfile),
-            {ok, RS1}
+    case rebar3:run(["grisp", "pack" | Args]) of
+        {error, _Reason} = Error -> Error;
+        {ok, _} -> {ok, RState}
     end.
 
 -spec expected_package_name(rebar_state:t()) -> list().
@@ -28,27 +20,6 @@ expected_package_name(RState) ->
     lists:flatten(lists:join(".", Elems)).
 
 %--- Internals -----------------------------------------------------------------
-
-rebar_command(RState, Namespace, Command, Args) ->
-    % Backup current command state
-    OriginalNamespace = rebar_state:namespace(RState),
-    OriginalArgs = rebar_state:command_args(RState),
-    OriginalParsedArgs = rebar_state:command_parsed_args(RState),
-
-    % Args are parsed by rebar_core
-    RebarState2 = rebar_state:namespace(RState, Namespace),
-    RebarState3 = rebar_state:command_args(RebarState2, Args),
-
-    case rebar_core:process_command(RebarState3, Command) of
-        {error, _Reason} = Error ->
-            Error;
-        {ok, RS} ->
-            % Restore current command state
-            RS2 = rebar_state:namespace(RS, OriginalNamespace),
-            RS3 = rebar_state:command_args(RS2, OriginalArgs),
-            RS4 = rebar_state:command_parsed_args(RS3, OriginalParsedArgs),
-            {ok, RS4}
-    end.
 
 release(RState) ->
     Relx = rebar_state:get(RState, relx, []),
